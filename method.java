c8ca115719bd30a4cf87e4e45d1f7cb2e4b46209
hanifakhatoon <hanifakhan.1994@gmail.com>
2025-01-21 14:01:47 -0800

    second file

---
+++ method.java
@@ -0,0 +1,5 @@
+package hanu;
+
+public class method {
+
+}
---
+++ muj.java
@@ -0,0 +1,8 @@
+package hanu;
+
+public class muj 
+{
+	public static void main(String[] args) {
+		System.out.println("helloooooo");
+	}
+}
